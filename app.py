59cdac777e0dcf871a587cf4a5daa834acd6dd71
amar deep singh <amardeep.singh0755@gmail.com>
2023-10-17 23:57:36 +0530

    second commit

--- app.py
+++ app.py
@@ -33,6 +33,31 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete=Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "there was a problem deleting that task"
+
+@app.route('/update/<int:id>',methods=['GET',"POST"])
+def update(id):
+    task=Todo.query.get_or_404(id)
+    if request.method=="POST":
+        task.content=request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "there is some problem with your update"
+        
+    
+    else:
+        return render_template('update.html',task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
